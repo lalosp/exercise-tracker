7cb5ca10609c627a1b9685bef2a166b3947a6e81
Eduardo Sandoval <ed.sand.pol@gmail.com>
2020-11-20 14:49:22 -0700

    deleted React boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-// What's the difference between import and require?
-import logo from './logo.svg';
-import './App.css';
+/* What's the difference between import and require?
+ * We're not going to use the logo or App.css
+ */
+//import logo from './logo.svg';
+//import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		// This JSX code is what gets rendered on the frontend
+    <div className="container">
+    	Hi, world!
+		</div>
   );
 }
 
